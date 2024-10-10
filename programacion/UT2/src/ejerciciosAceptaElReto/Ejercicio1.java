a4572470131a0acaf2d4215d2c3bed4ef95a99a9
Angel <angelajenjofernandez.4c@gmail.com>
2024-10-10 17:06:50 +0200

    10/10/24

--- programacion/UT2/src/ejerciciosAceptaElReto/Ejercicio1.java
+++ programacion/UT2/src/ejerciciosAceptaElReto/Ejercicio1.java
@@ -8,7 +8,8 @@ public class Ejercicio1 {
 		/*Nuestra tarea es hacer un programa que permita a nuestros niños saber si es Navidad o no.La entrada comenzará con un número N indicando cuántas fechas se consultarán. A continuación vendrán esas N fechas, con dos números enteros, día y mes en el formato DD MM indicando el día y el mes a consultar. Todas las fechas serán correctas.Por cada fecha de la entrada, el programa deberá escribir SI si la fecha es la del día de Navidad, 25 de diciembre, y NO en otro caso.*/
 		Scanner entrada = new Scanner(System.in);
 		int numeroFechas;
-		String fechas = "\n", navidad = "25 12", comodin;
+		String fechas = "\n", comodin;
+		final String NAVIDAD = "25 12";
 		
 		System.out.printf("Introduce cuantas fechas quieres evaluar: ");
 		numeroFechas = entrada.nextInt();
@@ -19,7 +20,7 @@ public class Ejercicio1 {
 			comodin = entrada.nextLine();
 			System.out.println("¿Ya es Navidad?");
 			
-			if (navidad.equalsIgnoreCase(comodin)) {
+			if (NAVIDAD.equalsIgnoreCase(comodin)) {
 				fechas += comodin + " " +"SI"+"\n";
 			} else {
 				fechas += comodin + " " + "NO"+"\n";
