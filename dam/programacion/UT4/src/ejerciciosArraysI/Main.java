4891dcd37c9242e9565c52713d4796d70a804a5e
Angel <angelajenjofernandez.4c@gmail.com>
2024-11-29 17:51:00 +0100

    29/11/24

--- dam/programacion/UT4/src/ejerciciosArraysI/Main.java
+++ dam/programacion/UT4/src/ejerciciosArraysI/Main.java
@@ -26,13 +26,12 @@ public class Main {
 		System.out.println("La tienda que menos vendio fue: " + menor);
 		System.out.println("La tienda que mas vendio fue: " + mayor);
 		System.out.println("La suma total de las ventas es: " + suma);
-		 */
 
 		
 		/*Almacenar en un vector 5 numeros enteros leidos por teclado. Leer a continuación otro
 		número y comprobar si está en el vector o no. En el caso de que esté visualizar que posición
 		ocupa; sino indicarlo mediante un mensaje. Visualizar tambien el elemento más pequeño, el
-		más grande y la posición de ambos.*/
+		más grande y la posición de ambos.
 		
 		Scanner entrada = new Scanner(System.in);
 		int numeros[] = new int[5], ultimoNumero = -1; 
@@ -79,8 +78,91 @@ public class Main {
 		
 		
 		
+		/*Diseñar una clase de nombre Test que nos permita representar una pregunta tipo test, la cual
+		estará compuesta de un enunciado, cuatro posibles respuestas y la respuesta correcta. Estas
+		tres propiedades privadas de la clase tendrán los nombres:
+		- Número de pregunta (int)
+		- enunciado.
+		- opciones. Array de 4 strings.
+		- valida. De tipo carácter. Guarda cual es la respuesta correcta
+		La clase tiene que implementar los métodos siguientes:
+		- setNumero, setEnunciado, setOpciones, setValida que asignarán cada uno de ellos lo valores
+		de cada una de las propiedades.
+		- getNumero, getEnunciado, getValida, getOpciones que devuelven de manera respectiva cada
+		una de las tres propiedades de la clase.
+		- Método de nombre presenta, que escribe de manera adecuada en pantalla la pregunta, y las
+		cuatro posibles respuestas, precedidas de las letras a, b, c, d.
+		Comprobar el funcionamiento de la clase Test declarando dos objetos de dicha clase, y evaluar
+		el funcionamiento de los métodos definidos.
 		
+		String opciones[] = {"Se levantará descansado", "Se levantará cansado", "No se levantará", "Comerá arroz"};
+		Test tema1 = new Test(1, "Si Adriano no respeta los ciclos del sueño que ocurrirá: ", opciones, 'B');
+		Scanner entrada = new Scanner(System.in);
+		char respuesta;
 		
+		tema1.presenta();
+		System.out.println("Por favor introduce tu respuesta: ");
+		respuesta = entrada.next().charAt(0);
+		
+		if (respuesta == tema1.getCorrecta()) {
+			System.out.println("Acertaste");
+		} else {
+			System.out.println("Has fallado");
+		}
+		
+		/*Hacer un programa en el que se declare un vector de 10 datos de tipo Test, asignándole valores
+		al declararlo. Plantear a continuación un examen con las 5 preguntas almacenadas en el vector
+		seleccionándolas de manera aleatoria y sin que haya preguntas repetidas. Se visualizará el
+		número de preguntas acertadas y falladas.
+		
+		//Esto lo omito con el for de la clase test
+		String opciones1[] = {"Se sentirá motivado", "Se sentirá desmotivado", "Tendrá hambre", "Estará aburrido"};
+		Test tema1 = new Test(1, "¿Qué sentirá Juan después de hacer ejercicio?", opciones1, 'B');
+
+		String opciones2[] = {"Se divertirá", "Se enojará", "Se cansará", "Se quedará dormido"};
+		Test tema2 = new Test(2, "¿Qué hará Pedro después de correr?", opciones2, 'C');
+
+		String opciones3[] = {"Gozará de buena salud", "Estará estresado", "Dormirá más tarde", "Estará más productivo"};
+		Test tema3 = new Test(3, "¿Qué beneficios se esperan si Ana duerme 8 horas?", opciones3, 'A');
+
+		String opciones4[] = {"Tendrá más energía", "Se sentirá cansado", "Se distraerá fácilmente", "No hará nada"};
+		Test tema4 = new Test(4, "¿Qué efecto tendrá el café en Luis?", opciones4, 'A');
+
+		String opciones5[] = {"Se olvidará de todo", "Recordará lo que estudió", "Se distraerá con música", "No aprenderá nada"};
+		Test tema5 = new Test(5, "¿Qué pasará si María estudia la noche anterior al examen?", opciones5, 'B');
+
+		String opciones6[] = {"Le dolerá la cabeza", "Estará relajado", "Tendrá hambre", "Sentirá calor"};
+		Test tema6 = new Test(6, "¿Qué pasará si Juan no toma suficiente agua?", opciones6, 'A');
+
+		String opciones7[] = {"Será muy puntual", "Llegará tarde", "Se olvidará del lugar", "Se sentirá nervioso"};
+		Test tema7 = new Test(7, "¿Qué pasará si Roberto no se prepara para su presentación?", opciones7, 'B');
+
+		String opciones8[] = {"Se despertará temprano", "Se quedará dormido", "Tendrá pesadillas", "Se despertará varias veces"};
+		Test tema8 = new Test(8, "¿Qué pasará si una persona tiene insomnio?", opciones8, 'C');
+
+		String opciones9[] = {"El clima será soleado", "Lloverá todo el día", "Habrá tormenta", "Estará nublado"};
+		Test tema9 = new Test(9, "¿Qué se espera para el clima mañana?", opciones9, 'D');
+
+		String opciones10[] = {"Se hará amigo de todos", "Se sentirá incómodo", "Tendrá éxito", "Se aburrirá"};
+		Test tema10 = new Test(10, "¿Qué hará Pedro en la fiesta?", opciones10, 'A');
+		
+		Test muestrario[] = {tema1, tema2, tema3, tema4, tema5, tema6, tema7, tema8, tema9, tema10};
+		
+		System.out.println(tema1.toString());
+		 */
+		
+		/*Hacer un programa en el que se declaren 3 vectores de nombres: nombre, apellido1 y
+		apellido2, que tengan cada uno de ellos 10 nombres y 10 apellidos, dándole valores al
+		declararlos sin que haya valores repetidos.
+		Declarar otro vector de nombre nombresApellidos de longitud 30, que contenga el contenido
+		de los 3 vectores anteriores. Primero irán los nombres y después los apellidos. Asignarle
+		valores a este vector utilizando el método arrayCopy.
+		a) Escribir a continuación el nombre y apellidos de 5 personas, eligiendo aleatoriamente los
+		valores de los vectores nombre, apellido1 y apellido2. Estos 5 valores tienen que almacenarse
+		previamente en un vector de 5 Strings de nombre nombresCompletos. Escribir los nombres
+		ordenados alfabéticamente
+		a) Repetir el apartado a) eligiendo los valores del vector nombresApellidos.
+		b) (opcional) Los nombres y apellidos no pueden estar repetidos.*/
 	}
 
 }
---
+++ dam/programacion/UT4/src/ejerciciosArraysI/Test.java
@@ -0,0 +1,154 @@
+package ejerciciosArraysI;
+
+import java.util.Arrays;
+
+public class Test {
+	//Atributos
+	private int nPregunta;
+	private String enunciado;
+	private String opciones[] = new String[4];
+	private char correcta;
+	
+	//Constructores
+	public Test() {
+		
+	}
+	
+	public Test(int numero, String enunciado, String opciones[], char correcta) {
+		nPregunta = numero;
+		this.enunciado = enunciado;
+		for (int i = 0; i < opciones.length; i++) {			//De esta forma creamos una copia, si los igualamos directamente ambos apuntaran a la misma celda de memoria!!!
+			this.opciones[i] = opciones[i];
+		}
+		this.correcta = correcta;
+	}
+	
+
+	//Metodos
+	public int getnPregunta() {
+		return nPregunta;
+	}
+
+	public void setnPregunta(int nPregunta) {
+		this.nPregunta = nPregunta;
+	}
+
+	public String getEnunciado() {
+		return enunciado;
+	}
+
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+
+	public String[] getOpciones() {
+		return opciones;
+	}
+
+	public void setOpciones(String[] opciones) {
+		this.opciones = opciones;
+	}
+
+	public char getCorrecta() {
+		return correcta;
+	}
+
+	public void setCorrecta(char correcta) {
+		this.correcta = correcta;
+	}
+	
+	public void presenta() {
+		String op1 = "", op2 = "", op3 = "", op4 = "";
+		for (int i = 0; i < opciones.length; i++) {
+			switch (i) {
+			case 0:
+				op1 = opciones[i];
+				break;
+			case 1:
+				op2 = opciones[i];	
+				break;
+			case 2:
+				op3 = opciones[i];	
+				break;
+			case 3:
+				op4 = opciones[i];
+				break;
+			}
+		}
+		String presenta = this.getnPregunta() + " " + this.getEnunciado() + "\n" + "\tA. " + op1 + "\n" + "\tB. " + op2 + "\n" + "\tC. " + op3 + "\n" + "\tC. " + op4;
+		System.out.println(presenta);
+	}
+	
+	public String presenta(Test t[]) {					//la idea es que coja de forma aleatoria 5 objetos del array 
+		int numeroTotalPreguntas = 5;					//posteriormente imprimir en orden dichos objetos
+		Test p1, p2, p3, p4, p5;						//objetos temporales para almacenar las preguntas que incluire en el examen
+		//Cambiar la linea superior por
+		Test bateriaPreg[];
+		for (int i = 0; i < 10; i++) {
+			bateriaPreg[i] = new Test(); 
+		}
+		//y me ahorro crear cada objeto en el main, al estar dentro de un array no necesitan un nombre
+		
+		for (int i = 0; i < numeroTotalPreguntas; i++) {
+			int r = (int)(Math.random()*5) + 1;
+			
+			switch (i) {
+			case 0:
+				p1 = t[r];
+				break;
+			case 1:
+				p2 = t[r];	
+				break;
+			case 2:
+				p3 = t[r];	
+				break;
+			case 3:
+				p4 = t[r];
+				break;
+			case 4:
+				p5 = t[r];
+			}
+			
+		}
+		return "";										//Aqui debo copiar el return del otro metodo
+	}
+
+
+	@Override
+	public String toString() {
+		return "Test \n" + nPregunta + ". " + enunciado + "\n" + Arrays.toString(opciones)
+				;
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
