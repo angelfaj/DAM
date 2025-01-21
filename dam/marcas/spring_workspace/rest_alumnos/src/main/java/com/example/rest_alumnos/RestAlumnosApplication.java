faa2e802576638e2ddb06b75770eb51575664dfc
Angel <angelajenjofernandez.4c@gmail.com>
2025-01-21 17:01:16 +0100

    21/1/25

---
+++ dam/marcas/spring_workspace/rest_alumnos/src/main/java/com/example/rest_alumnos/RestAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAlumnosApplication.class, args);
+	}
+
+}
---
+++ dam/marcas/spring_workspace/rest_alumnos/src/test/java/com/example/rest_alumnos/RestAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
