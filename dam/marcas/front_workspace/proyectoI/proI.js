76ca0f2f63daca83ce61384f36ee5f359b890395
Angel <angelajenjofernandez.4c@gmail.com>
2025-02-11 17:08:44 +0100

    11/2/25

--- dam/marcas/front_workspace/proyectoI/proI.js
+++ dam/marcas/front_workspace/proyectoI/proI.js
@@ -1,4 +1,28 @@
-function hola() {
-    alert("Hola mundo");
-    console.log("Hola mundo");
+var contador = 0;
+
+
+window.onload = function(){
+    incrementar = document.getElementById("incrementButon");
+    incrementar.addEventListener("click", suma);
+    decrementar = document.getElementById("decrementButon");
+    decrementar.addEventListener("click", resta);
+    setContador = document.getElementById("initButon");
+    setContador.addEventListener("click", inicializar);
 }
+
+function suma() {
+    contador++;
+    //alert("Hola mundo");
+    console.log("El contador vale: " + contador);
+}
+
+function resta() {
+    contador--;
+    //alert("Hola mundo");
+    console.log("El contador vale: " + contador);
+}
+
+function inicializar() {
+    contador = document.getElementById("initButon").value;
+}
+
