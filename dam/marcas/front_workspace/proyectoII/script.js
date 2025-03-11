73e6a3ab6e24e45c30700f5fa4d38fea157377b7
Angel <angelajenjofernandez.4c@gmail.com>
2025-03-11 17:10:18 +0100

    11/3/25

--- dam/marcas/front_workspace/proyectoII/script.js
+++ dam/marcas/front_workspace/proyectoII/script.js
@@ -14,3 +14,23 @@ function addAlumno() {          //Esta funcion añade un alumno a nuestro servid
         }    
     )           
 }
+
+function validar() {
+    var valoresCorrectos = true;
+
+    if (document.getElementById("name").value.length < 2) {
+        valoresCorrectos = false;
+    }
+    if (document.getElementById("id_alumno").value < 1) {
+        valoresCorrectos = false;
+    }
+    if (document.getElementById("asignatura").length < 2) {
+        valoresCorrectos = false;
+    }
+
+    if (valoresCorrectos) {
+        addAlumno();
+    }else {
+        alert("los datos no cumplen con los requisitos");
+    }
+}
