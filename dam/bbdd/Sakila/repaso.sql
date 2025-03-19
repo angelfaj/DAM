503fd05f217bdccf45cd75555c52302cce3df947
Angel <angelajenjofernandez.4c@gmail.com>
2025-03-19 12:52:50 +0100

    19/3/25

--- dam/bbdd/Sakila/repaso.sql
+++ dam/bbdd/Sakila/repaso.sql
@@ -29,4 +29,62 @@ CREATE VIEW bigger_replacement_cost_view AS
 	SELECT a.first_name, a.last_name FROM actor a UNION SELECT c.first_name, c.last_name FROM customer c;
 */
 
+/* 3. TRIGGERS
+ trigger que no permita alquilar a customers cuyo nombre no empiece por a, ademas debe mostrar un mensaje y enviar la señal de estado 45000
+ */
+ /*
+ DROP TRIGGER IF EXISTS customer_without_a_cant_rent;
+ DELIMITER //
+ CREATE TRIGGER customer_without_a_cant_rent 
+ BEFORE INSERT ON rental
+ FOR EACH ROW
+ BEGIN
+	DECLARE customer_name VARCHAR(1);
+    SET customer_name = (
+		SELECT SUBSTRING(first_name, 1, 1) FROM customer WHERE customer_id = new.customer_id);
+    
+    if !STRCMP(customer_name, 'A') then 
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Customer name must start with A';
+	end if;
+ END//
+ DELIMITER ;
+ 
+ INSERT INTO rental (customer_id, inventory_id, staff_id) values (29, 3, 2);
+ 	SELECT customer_id, first_name FROM customer WHERE SUBSTRING(first_name, 1, 1) like 'A';
+ */
+ 
+ /*trigger que controle la insercion en payment para que el amount sea positivo con mismo mensaje que el anterior*/
+ /*
+ DROP TRIGGER IF EXISTS positive_amount_trigger;
+ DELIMITER //
+ CREATE TRIGGER positive_amount_trigger
+ BEFORE INSERT ON payment
+ FOR EACH ROW
+ BEGIN
+	if new.amount < 0 then 
+		signal sqlstate '45000' set MESSAGE_TEXT = 'Amount must be a positive number';
+	end if;
+ END//
+ DELIMITER ;
+ 
+ INSERT INTO payment (customer_id, staff_id, rental_id, amount)
+	VALUES (4, 2, 5, -1);
+*/
 
+/*trigger que desactive las cuentas con inactividad mayor o igual a seis meses(180dias).*/
+/*
+DROP TRIGGER IF EXISTS disable_acount_trigger;
+DELIMITER //
+CREATE TRIGGER disable_acount_trigger
+BEFORE UPDATE ON customer
+FOR EACH ROW
+BEGIN	
+    if NOW() - INTERVAL 6 MONTH > old.last_update then
+		SET new.active = 0;
+	end if;
+END//
+DELIMITER ;
+
+update customer set last_name='pure' where customer_id = 1;
+select * from customer where customer_id = 1;
+*/
