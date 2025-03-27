4bb1caae597750f3c0c5934bb2f23e31ba05ef2a
Angel <angelajenjofernandez.4c@gmail.com>
2025-03-27 19:10:27 +0100

    27/3/25

--- dam/bbdd/Sakila/excepciones/excepciones-transacciones.sql
+++ dam/bbdd/Sakila/excepciones/excepciones-transacciones.sql
@@ -1,4 +1,5 @@
 -- procedimiento para capturar excepciones y realizar las gestiones sobre una transaccion
+/*
 DELIMITER //
 CREATE PROCEDURE error_handler()
 BEGIN
@@ -21,3 +22,37 @@ BEGIN
     
 END//
 DELIMITER ;
+*/
+
+/*
+0 FALSO, 1 TRUE. Los clientes que no haya alquilado peliculas en el ultimo año, lo ponemos en inativo(0)*/
+DROP PROCEDURE IF EXISTS inactive_customer;
+DELIMITER //
+CREATE PROCEDURE inactive_customer()
+BEGIN
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+		SELECT 'Ocurrio un error, ejecutando rollback' AS MESSAGE;
+        ROLLBACK;
+    END;
+    
+    DECLARE CONTINUE HANDLER FOR SQLWARNING
+    BEGIN
+		SELECT 'Ocurrio una advertencia, ejecutando rollback' AS MESSAGE;
+        ROLLBACK;
+    END;
+    
+    START TRANSACTION;
+    IF DATEDIFF(NOW(), (SELECT return_date FROM rental)) > 365 THEN
+		UPDATE customer SET active = 0 where customer_id = new.customer_id;
+	END IF;
+    COMMIT;
+    
+END//
+DELIMITER ;
+call inactive_customer();
+SELECT active from customer;
+FOR EACH ROW SELECT DATEDIFF(NOW(), return_date)  FROM rental;
+
+
+/* rEALIZAR REBAJA DEL 10% A AQUELLAS PELIS NO ALQUILADAS EN LOS 6 ULTIMOS MESES
