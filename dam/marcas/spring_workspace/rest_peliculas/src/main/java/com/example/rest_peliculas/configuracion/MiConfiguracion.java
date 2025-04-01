192f3f73ab85c326bfa4b8456abb30754a82e8b1
Angel <angelajenjofernandez.4c@gmail.com>
2025-04-01 17:08:39 +0200

    1/4/25

--- dam/marcas/spring_workspace/rest_peliculas/src/main/java/com/example/rest_peliculas/configuracion/MiConfiguracion.java
+++ dam/marcas/spring_workspace/rest_peliculas/src/main/java/com/example/rest_peliculas/configuracion/MiConfiguracion.java
@@ -1,5 +1,6 @@
 package com.example.rest_peliculas.configuracion;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,4 +23,18 @@ public class MiConfiguracion {
 		
 		return peliculas;
 	}
+	
+	@Bean						//Esta anotacion de spring hace que el objeto este disponible en cualquier parte del proyecto
+	public HashMap<Integer, Pelicula> get_alumnos_hash() {
+		HashMap<Integer, Pelicula> peliculas = new HashMap<Integer, Pelicula>();
+		Pelicula p1 = new Pelicula(1, 1995, "Seven", "Andrew Kevin", "Cine negro");
+		Pelicula p2 = new Pelicula(2, 2010, "Inception", "Christopher Nolan", "Ciencia ficcion");
+		Pelicula p3 = new Pelicula(3, 2004, "Troya", "David Benioff", "Epico");
+		peliculas.put(1, p1);
+		peliculas.put(2, p2);
+		peliculas.put(3, p3);
+		
+		return peliculas;
+	}
+	
 }
