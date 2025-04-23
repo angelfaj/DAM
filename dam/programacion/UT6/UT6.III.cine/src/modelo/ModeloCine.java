196f13b2e45a8f02cbba7a5e723f9cb5d65fbad5
Angel <angelajenjofernandez.4c@gmail.com>
2025-04-23 21:17:38 +0200

    23/4/25

--- dam/programacion/UT6/UT6.III.cine/src/modelo/ModeloCine.java
+++ dam/programacion/UT6/UT6.III.cine/src/modelo/ModeloCine.java
@@ -1,5 +1,110 @@
 package modelo;
 
-public class ModeloCine {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
+public class ModeloCine implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String film;
+	private boolean disabledPerson;
+	private int seat;
+	private static final File facturaBin = new File("factura.bin"); 
+	private static final File facturaTxt= new File("factura.txt"); 
+	
+	public ModeloCine() {
+		super();
+	}
+	
+	public ModeloCine(String email, String film, boolean disabledPerson, int seat) {
+		super();
+		this.email = email;
+		this.film = film;
+		this.disabledPerson = disabledPerson;
+		this.seat = seat;
+	}
+
+	public void saveFactura() throws IOException {
+		//Guardfamos en el txt
+		FileWriter writer = null;
+		
+		if (!facturaTxt.exists()) {
+			writer = new FileWriter(facturaBin);
+		}else {
+			writer = new FileWriter(facturaBin, true);
+		}
+		writer.write(this + "");
+		writer.close();
+		
+		//Guardfamos en el bin
+		FileOutputStream fo = null;
+		ObjectOutputStream datos = null;
+		
+		if (!facturaBin.exists()) {
+			fo = new FileOutputStream(facturaBin);
+			datos = new ObjectOutputStream(fo);
+		}else {
+			fo = new FileOutputStream(facturaBin, true);
+			datos = new MiObjectOutputStream(fo);
+		}
+		
+		datos.writeObject(this);
+		datos.close();
+	}
+	
+	public String showTxt() {
+		String txt = "", line = "";
+		
+		return txt;
+	}
+	
+	public String showBin() {
+		String txt = "";
+		
+		return txt;
+	}
+	
+	@Override
+	public String toString() {
+		return "Reserva [email=" + email + ", film=" + film + ", disabledPerson=" + disabledPerson + ", seat=" + seat
+				+ "]";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFilm() {
+		return film;
+	}
+
+	public void setFilm(String film) {
+		this.film = film;
+	}
+
+	public boolean isDisabledPerson() {
+		return disabledPerson;
+	}
+
+	public void setDisabledPerson(boolean disabledPerson) {
+		this.disabledPerson = disabledPerson;
+	}
+
+	public int getSeat() {
+		return seat;
+	}
+
+	public void setSeat(int seat) {
+		this.seat = seat;
+	}
+	
+	
 }
--- dam/programacion/UT6/UT6.III.cine/src/vista/VistaCine.java
+++ dam/programacion/UT6/UT6.III.cine/src/vista/VistaCine.java
@@ -5,9 +5,13 @@ import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
-import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.JLabel;
 
 public class VistaCine extends JFrame {
 
@@ -16,6 +20,12 @@ public class VistaCine extends JFrame {
 	private JTextField textFieldEmail;
 	private JRadioButton rdbtnSi, rdbtnNo;
 	private ButtonGroup rdbtnGroup;
+	private DefaultTableModel modeloTabla;
+	private JTable table;
+	private JButton btnNewEnviar;
+	private JComboBox<String> comboBoxFilms;
+	private static final String[] PELICULAS = {"La naranja mecánica", "El resplandor", "Solo en casa", "Acompañado en casa"};
+	private static final String[] columnas = {"F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10"};
 	
 	public VistaCine() {
 		setTitle("Compra de butacas");
@@ -29,25 +39,95 @@ public class VistaCine extends JFrame {
 		
 		textFieldEmail = new JTextField();
 		textFieldEmail.setSize(151, 20);
-		textFieldEmail.setLocation(21, 48);
+		textFieldEmail.setLocation(21, 113);
 		contentPane.add(textFieldEmail);
 		textFieldEmail.setColumns(10);
 		
 		
 		rdbtnNo = new JRadioButton("No");
-		rdbtnNo.setBounds(21, 120, 50, 23);
+		rdbtnNo.setBounds(21, 169, 50, 23);
 		contentPane.add(rdbtnNo);
 		
 		rdbtnSi = new JRadioButton("Si");
-		rdbtnSi.setBounds(100, 120, 50, 23);
+		rdbtnSi.setBounds(100, 169, 50, 23);
 		contentPane.add(rdbtnSi);
 
 		rdbtnGroup = new ButtonGroup();
 		rdbtnGroup.add(rdbtnSi);
 		rdbtnGroup.add(rdbtnNo);
 		
+		btnNewEnviar = new JButton("Enviar");
+		btnNewEnviar.setBounds(180, 208, 89, 23);
+		contentPane.add(btnNewEnviar);
+		
+		comboBoxFilms = new JComboBox<String>(PELICULAS);
+		comboBoxFilms.setBounds(21, 54, 152, 28);
+		contentPane.add(comboBoxFilms);
+		
+		modeloTabla = new DefaultTableModel(null, columnas);
+		
+		table = new JTable(modeloTabla);
+		table.setBounds(251, 54, 151, 138);
+		contentPane.add(table);
+		
+		JLabel lblNewLabel = new JLabel("Selecciona la película:");
+		lblNewLabel.setBounds(21, 30, 151, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblIntroduceTuEmail = new JLabel("Introduce tu email:");
+		lblIntroduceTuEmail.setBounds(21, 93, 151, 14);
+		contentPane.add(lblIntroduceTuEmail);
+		
+		JLabel lblPersonaConMovilidad = new JLabel("Persona con movilidad reducida:");
+		lblPersonaConMovilidad.setBounds(21, 148, 192, 14);
+		contentPane.add(lblPersonaConMovilidad);
+		
+		JLabel lblSeleccionaLaButaca = new JLabel("Selecciona la butaca:");
+		lblSeleccionaLaButaca.setBounds(251, 29, 151, 14);
+		contentPane.add(lblSeleccionaLaButaca);
+		
 		
 		setVisible(true);
 		setResizable(false);
+	}
+
+	public void fillTable(String[][] datosMatriz) {
+		modeloTabla.setRowCount(0); //Limpiamos la tabla
+		
+		for (String[] fila : datosMatriz) {
+			modeloTabla.addRow(fila);
+		}
+	}
+	
+	public JTextField getTextFieldEmail() {
+		return textFieldEmail;
+	}
+
+	public JRadioButton getRdbtnSi() {
+		return rdbtnSi;
+	}
+
+	public JRadioButton getRdbtnNo() {
+		return rdbtnNo;
+	}
+
+	public ButtonGroup getRdbtnGroup() {
+		return rdbtnGroup;
+	}
+
+	public DefaultTableModel getModeloTabla() {
+		return modeloTabla;
+	}
+
+	public JTable getTable() {
+		return table;
+	}
+
+	public JButton getBtnNewEnviar() {
+		return btnNewEnviar;
+	}
+
+	public JComboBox<String> getComboBoxFilms() {
+		return comboBoxFilms;
 	}
 }
