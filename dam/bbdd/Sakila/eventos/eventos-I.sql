770542cd50e57f2e6157261c05d77e7ef2d394d4
Angel <angelajenjofernandez.4c@gmail.com>
2025-05-08 17:04:49 +0200

    8/5/25

--- dam/bbdd/Sakila/eventos/eventos-I.sql
+++ dam/bbdd/Sakila/eventos/eventos-I.sql
@@ -74,27 +74,119 @@
 
 -- 4. Crear un evento que corra una sola vez dentro de 10 minutos e inserte una fila
 -- en log_eventos.
+DROP EVENT IF EXISTS insert_once_event;
 
+CREATE EVENT IF NOT EXISTS insert_once_event
+ON SCHEDULE AT NOW() + INTERVAL 10 MINUTE
+DO INSERT INTO log_eventos VALUES (NOW());
 
 -- 5. Crear un evento que borre registros de payment donde amount = 0 una vez al
 -- día.
+DROP EVENT IF EXISTS delete_payment_with_cero_amount_event;
+
+CREATE EVENT IF NOT EXISTS delete_payment_with_cero_amount_event
+ON SCHEDULE EVERY 24 HOUR
+DO DELETE FROM payment WHERE amount = 0;
+
 -- 6. Crear un evento deshabilitado por defecto.
+DROP EVENT IF EXISTS disable_event;
+
+CREATE EVENT IF NOT EXISTS disable_event
+ON SCHEDULE AT NOW() + INTERVAL 20 HOUR 
+DISABLE DO DELETE FROM payment WHERE amount = 0;
+
 -- 7. Listar todos los eventos definidos en la base de datos Sakila.
--- Nivel Intermedio (8–14) – Uso con tablas Sakila reales
+SHOW events;
+
 -- 8. Crear un evento que, cada semana, cuente cuántas películas están alquiladas
 -- (rental) y lo registre en log_eventos.
+ALTER TABLE log_eventos ADD 
+	films_on_rent SMALLINT UNSIGNED;
+
+DROP EVENT IF EXISTS count_films_on_rent;
+
+CREATE EVENT IF NOT EXISTS count_films_on_rent
+ON SCHEDULE EVERY 1 WEEK DO
+INSERT INTO log_eventos (films_on_rent)
+	SELECT COUNT(film_id) FROM inventory i
+    JOIN rental r ON i.inventory_id = r.inventory_id 
+	WHERE r.return_date IS NULL;
+    
 -- 9. Crear un evento que actualice el campo last_update de todos los actores una
 -- vez por día.
+DROP EVENT IF EXISTS update_actor_last_update_event;
+
+CREATE EVENT IF NOT EXISTS update_actor_last_update_event
+ON SCHEDULE EVERY 1 DAY
+DO UPDATE actor SET last_update = NOW();
+
 -- 10. Crear un evento que borre registros antiguos de la tabla rental (más de 5
 -- años).
+DROP EVENT IF EXISTS delete_old_rental_records;
+
+CREATE EVENT IF NOT EXISTS delete_old_rental_records
+ON SCHEDULE AT NOW + INTERVAL 5 YEAR
+DO DELETE FROM rental WHERE (YEAR(last_update) - YEAR(NOW())) >= 5;
+
 -- 11. Crear una tabla log_bajas y un evento que mueva clientes inactivos de
 -- customer allí cada mes.
--- 12. Crear un evento que actualice automáticamente el estado de los pagos
--- (payment) con fecha futura a “pendiente”.13. Crear un evento que actualice cada día los precios de películas de más de 10
+DROP TABLE IF EXISTS log_bajas;
+CREATE TABLE IF NOT EXISTS log_bajas (
+  `customer_id` smallint unsigned NOT NULL PRIMARY KEY,
+  `store_id` tinyint unsigned NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `address_id` smallint unsigned NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `create_date` datetime NOT NULL,
+  `last_update` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+DROP EVENT IF EXISTS move_inactive_customer_event;
+DELIMITER //
+CREATE EVENT IF NOT EXISTS move_inactive_customer_event
+ON SCHEDULE AT NOW() + INTERVAL EVERY MONTH
+DO BEGIN
+	INSERT INTO log_bajas SELECT * FROM customer WHERE active = false;
+    DELETE FROM customer WHERE active = false;    
+END//
+DELIMITER ;
+
+-- 12. Crear un evento que actualice automáticamente el estado de los pagos (payment) 
+-- con fecha futura a “pendiente”.
+-- OMITIDO
+
+-- 13. Crear un evento que actualice cada día los precios de películas de más de 10
 -- años con un 10% de descuento.
+DROP EVENT IF EXISTS update_rental_rate_to_old_film_event;
+
+CREATE EVENT IF NOT EXISTS update_rental_rate_to_old_film_event
+ON SCHEDULE AT NOW() + INTERVAL EVERY DAY
+DO UPDATE film SET rental_rate = rental_rate*0.9 
+WHERE (SELECT * FROM film 
+		WHERE (YEAR(NOW()) - release_year) >= 10);
+
+
 -- 14. Crear un evento que inserte estadísticas de uso (películas más alquiladas)
 -- cada semana en una tabla de resumen.
--- Nivel Avanzado (15–20) – Tareas automatizadas más realistas
+ALTER TABLE log_eventos ADD most_rented_film SMALLINT UNSIGNED;
+
+DROP EVENT IF EXISTS save_most_rented_film_event;
+
+CREATE EVENT IF NOT EXISTS save_most_rented_film_event
+ON SCHEDULE AT NOW() + INTERVAL EVERY WEEK
+DO 
+INSERT INTO log_eventos(most_rented_film) 
+	SELECT i.film_id FROM inventory i 
+    JOIN rental r ON r.inventory_id = i.inventory_id
+    WHERE i.film_id IN (
+		SELECT i.film_id as film_id, COUNT(i.film_id) FROM inventory i 
+		JOIN rental r ON r.inventory_id = i.inventory_id 
+
+!!SELECT SOBRE LA SUBCONSULTA PARA NO REPETIR CODIGO 
+
+
 -- 15. Crear un evento que ejecute un procedimiento almacenado que calcula
 -- ingresos mensuales y lo guarda en una tabla reporte_mensual.
 -- 16. Crear un evento que, al finalizar el mes, archive la tabla payment y vacíe los
