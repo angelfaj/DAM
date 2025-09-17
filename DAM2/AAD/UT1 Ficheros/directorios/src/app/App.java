5b4f0370886c90b39e25b13fc068821e78d126c7
angelfaj <johndoe.smr@gmail.com>
2025-09-17 13:07:49 +0200

    17/9/25

--- DAM2/AAD/UT1 Ficheros/directorios/src/app/App.java
+++ DAM2/AAD/UT1 Ficheros/directorios/src/app/App.java
@@ -6,24 +6,34 @@ import java.io.IOException;
 public class App {
 
 	public static void main(String[] args) {
-		// Programa a modo de script para crear una estructura de directorios determinada
+		// Programa a modo de script para crear una estructura de directorios determinada: Creara un archivo dentro de cada directoro del path dado. /ejemplo/raiz/carpeta1/archivoNuevo, /ejemplo/raiz/carpeta2/archivoNuevo ...
 		
-		String path = "";
+		String path = "introduce aqui la ruta raiz";
+		String filename = "introduce aqui el nombre del archivo";
 		
+		System.out.println("Lista de directorios:");
+		elEnumerador(path);
+		try {
+			elOrquestador(path, filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
-	public static void elHacedor(String fileName) throws IOException {
-		File file = new File(fileName);
+	public static void elHacedor(File file) throws IOException {	//Crea el archivo pasado por parametro
 		if (!file.exists()) {
 			file.createNewFile();
 			System.out.println("archivo creado");
 		}else System.out.println("el archivo ya existe");
 	}
 	
-	public static File[] elEnumerador(String path) {
+	public static File[] elEnumerador(String path) {	//Imprime y devuelve todos los archivos/directorios dentro del path dado
 		File raiz = new File(path);
-		if (raiz.isFile()) {
+		if (!raiz.exists()) {
+			System.out.println("[ERROR]-la ruta ("+path+") no existe");
+			return null;
+		}else if (raiz.isFile()) {
 			System.out.println("[ERROR]-la raiz es un archivo");
 			return null;
 		}else {
@@ -37,7 +47,10 @@ public class App {
 		}
 	}
 	
-	public static void elCombinador() {
-		
+	public static void elOrquestador(String pathRaiz, String fileName) throws IOException {	//Combina ambos metodos para crear la estructura deseada
+		File[] dirs = elEnumerador(pathRaiz);
+		for (File f:dirs) {
+			elHacedor(new File(f.getAbsolutePath(), fileName));
+		}
 	}
 }
