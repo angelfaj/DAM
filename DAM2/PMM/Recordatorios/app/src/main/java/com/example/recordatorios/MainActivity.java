486be5b557a730687ae47960d82790ab191e5f56
vespertino <vespertino@max12>
2025-11-04 18:29:54 +0100

    4/11/25

--- DAM2/PMM/Recordatorios/app/src/main/java/com/example/recordatorios/MainActivity.java
+++ DAM2/PMM/Recordatorios/app/src/main/java/com/example/recordatorios/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.recordatorios;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 // La idea es crear una app que sirva como recordatorios en la que poder añadir o eliminar tareas, pudiento mostrar las marcadas como hechas y las pendientes
+   private ListView listaTareas;
+   private Button hechasBtn;
+   private Button pendientesBtn;
+   private Button addBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        listaTareas = findViewById(R.id.listaPrincipal);
+        hechasBtn = findViewById(R.id.hechas_btn);
+        pendientesBtn = findViewById(R.id.pendientes_btn);
+        addBtn = findViewById(R.id.add_btn);
+    }
+
+    public void addListeners() {
+        listaTareas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
