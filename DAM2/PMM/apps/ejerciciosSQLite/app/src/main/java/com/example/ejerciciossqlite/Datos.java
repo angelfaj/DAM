09405057072445b400f865c3566a56115caebaf3
vespertino <vespertino@max12>
2026-01-14 16:48:59 +0100

    14/1/26

--- DAM2/PMM/apps/ejerciciosSQLite/app/src/main/java/com/example/ejerciciossqlite/Datos.java
+++ DAM2/PMM/apps/ejerciciosSQLite/app/src/main/java/com/example/ejerciciossqlite/Datos.java
@@ -3,6 +3,7 @@ package com.example.ejerciciossqlite;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -13,14 +14,15 @@ public class Datos extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String crearTabla = "CREATE TABLE departamento(idDep INTEGER PRIMARY KEY, nombre VARCHAR(30))";
+        //String crearTabla = "CREATE TABLE departamento(idDep INTEGER PRIMARY KEY, nombre VARCHAR(30))";
+        String crearTabla = "CREATE TABLE jugador(idDep INTEGER PRIMARY KEY, nombre VARCHAR(30))";
         db.execSQL(crearTabla);
-        System.out.println("Tabla departamento creada");
+        Log.i("MI_APP","Tabla jugador creada");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        System.out.println("Base de datos actualizada correctamente");
+        Log.i("MI_APP", "Base de datos actualizada correctamente");
     }
 
     public void insertarFilasXDefecto(SQLiteDatabase db) {
@@ -31,5 +33,18 @@ public class Datos extends SQLiteOpenHelper {
             String insertTemp = insert + idDep + ", '" + nombre + idDep + "')";
             db.execSQL(insertTemp);
         }
+        Log.i("MI_APP","Filas insertadas");
+    }
+
+    public void ejecutarSQL(SQLiteDatabase db){
+        int idDep;
+        String nombre = "jugador-";
+        String insert = "INSERT INTO jugador(idDep, nombre) VALUES(";
+        for (idDep = 1; idDep <= 4; idDep++) {
+            String insertTemp = insert + idDep + ", '" + nombre + idDep + "')";
+            db.execSQL(insertTemp);
+        }
+        Log.i("MI_APP","Filas insertadas en la tabla jugador");
+
     }
 }
--- DAM2/PMM/apps/ejerciciosSQLite/app/src/main/java/com/example/ejerciciossqlite/MainActivity.java
+++ DAM2/PMM/apps/ejerciciosSQLite/app/src/main/java/com/example/ejerciciossqlite/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ejerciciossqlite;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +15,50 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity{
 
-    /*
-    EJERCICIO 1:
-    * Crear una BD en un contexto dado con una unica tabla con dos campos (entero y cadena)
-    * Insertar 4 filas
-    * Informar sin la BD ha sido creada o se ha producido algun error
-    * Descargar la BD y comprobar que se ha creado correctamente con SQLite/Fiddle
-    * Por ultimop actualizar la base de datos a la version 2
-     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        /*EJERCICIO 1:
+        * Crear una BD en un contexto dado con una unica tabla con dos campos (entero y cadena)
+        * Insertar 4 filas
+        * Informar sin la BD ha sido creada o se ha producido algun error
+        * Descargar la BD y comprobar que se ha creado correctamente con SQLite/Fiddle
+        * Por ultimop actualizar la base de datos a la version 2*/
+        /*
         Datos datos = new Datos(this, "test", null, 1);
         SQLiteDatabase testDb = datos.getWritableDatabase();
 
         datos.insertarFilasXDefecto(testDb);
+        testDb.close();
+        */
+        /*EJERCICIO 2:
+        * Aplicación para la gestión de bases de datos en la que se pueda:
+        * Crear una base de datos a partir de su nombre escrito en un EditText.
+        * Escribir sentencias SQL que no devuelvan resultados y que se ejecuten sobre la BD seleccionada (crear/eliminar tablas, insertar/actualizar/eliminar datos).
+        * Para ello debes crear una función ejecutarSQL en la clase que gestione la bbdd (herede de SQLiteOpenHelper) que realice la ejecución de las sentencias escritas por el usuario.
+        * Mediante mensajes se deberá informar de los errores que ocurran durante la ejecución.*/
 
+        EditText entrada = findViewById(R.id.editText);
+        Button btn = findViewById(R.id.crearBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombreDb = entrada.getText().toString().trim();
+                crearDB(MainActivity.this, nombreDb);
+            }
+        });
+
+    }
+
+    private  void crearDB(Context c, String nombreDB) {
+        Datos d = new Datos(c, nombreDB, null, 1);
+        SQLiteDatabase db = d.getWritableDatabase();
+        d.ejecutarSQL(db);
+        db.close();
     }
 }
