eca27960e731fd5854c7b0b1cca7f2f7dd836d50
angel <johndoe.smr@gmail.com>
2026-01-26 11:57:13 +0100

    26/1/26

--- DAM2/AAD/UT4ddbb_objeto_relacionales/neodatis/actividadesII/src/app/App.java
+++ DAM2/AAD/UT4ddbb_objeto_relacionales/neodatis/actividadesII/src/app/App.java
@@ -35,17 +35,41 @@ public class App {
 		
 //		7. Obtener jugadores de FRANCIA o ITALIA (OR con múltiples valores)
 //		JugadorDAO.getFrenchOrItalian();
+
+		
+//		0. Consultar la información de todos los jugadores y sus países.
+//		JugadorDAO.getJugadorFromOID(7);
+//		JugadorDAO.getPlayersAndCountries();
+		
+//		1. Consultar jugadores mayores de 20 años y que practiquen fútbol (AND)
+//		JugadorDAO.getAbove20FootbalPlayers();
+		
+//		2. Consultar jugadores que sean de FRANCIA o jueguen baloncesto (OR)
+//		JugadorDAO.getFrenchOrBasketPayers();
+		
+//		3. Contar cuántos jugadores hay por cada deporte. (COUNT+GROUP BY)
+//		JugadorDAO.getTotalPlayersBySport();
+		
+//		4. Calcular la edad promedio de los jugadores
+//		JugadorDAO.getAvgPlayersAge();
+
+//		5. Obtener la edad mínima y máxima de los jugadores	
+//		JugadorDAO.getMinMaxPlayersAge();
+		
+//		6. Obtener jugadores que juegan fútbol y tienen entre 18 y 25 años (AND con rango de valores)
+//		JugadorDAO.getFootballBtw18And25();
+		
+//		7. Obtener jugadores de FRANCIA o ITALIA (OR con múltiples valores)
+//		JugadorDAO.getFrenchOrIta();
 		
 //		8. Obtener jugadores que juegan baloncesto y no tienen más de 22 años (AND+ NOT)
-//		JugadorDAO.getBasketUnder22();
-		
+//		JugadorDAO.getBasketUnder23();
 		
 //		9. Contar jugadores que son de FRANCIA o practican tenis (COUNT + OR)
-//		JugadorDAO.getFrenchOrTennis();
-		
+//		JugadorDAO.getFrenchOrTennisCount();
 		
 //		10. Obtener la edad promedio de jugadores que juegan tenis o tienen más de 22 años (AVG + OR)
-//		JugadorDAO.getAvgTennisOrMoreThan22();
+//		JugadorDAO.getAvgTenisOrAbove22();
 	}
 
 }
--- DAM2/AAD/UT4ddbb_objeto_relacionales/neodatis/actividadesII/src/dao/JugadorDAO.java
+++ DAM2/AAD/UT4ddbb_objeto_relacionales/neodatis/actividadesII/src/dao/JugadorDAO.java
@@ -2,9 +2,12 @@ package dao;
 
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
+import org.neodatis.odb.OID;
 import org.neodatis.odb.ObjectValues;
 import org.neodatis.odb.Objects;
 import org.neodatis.odb.Values;
+import org.neodatis.odb.core.oid.OIDFactory;
+import org.neodatis.odb.core.query.IQuery;
 import org.neodatis.odb.core.query.criteria.And;
 import org.neodatis.odb.core.query.criteria.ICriterion;
 import org.neodatis.odb.core.query.criteria.Or;
@@ -13,176 +16,342 @@ import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
 import org.neodatis.odb.impl.core.query.values.ValuesCriteriaQuery;
 
 import model.Jugador;
+import model.Paises;
 
 public class JugadorDAO {
 	
 	public static void getPlayersAndCountries() {
 		ODB odb = ODBFactory.open("equipos.db");
-		Objects<Jugador> jugadores = null;
+		Objects<Jugador> jugadores = odb.getObjects(Jugador.class);
 		
-		jugadores = odb.getObjects(Jugador.class);
-		while(jugadores.hasNext()) {
+		while (jugadores.hasNext()) {
+			System.out.println(jugadores.next());
+		}
+		odb.close();
+	}
+	
+	public static void getAbove20FootbalPlayers() {
+		ODB odb = ODBFactory.open("equipos.db");
+		ICriterion above20FooT = new And().add(Where.equal("deporte", "fútbol")).add(Where.gt("edad", 20));
+		IQuery above20FooTQuery = new CriteriaQuery(Jugador.class, above20FooT);
+		
+		Objects<Jugador> jugadores = odb.getObjects(above20FooTQuery);
+		while (jugadores.hasNext()) {
 			System.out.println(jugadores.next());
 		}
 		
 		odb.close();
 	}
 	
-	public static void getFootballAbove20() {
+	public static void getFrenchOrBasketPayers() {
 		ODB odb = ODBFactory.open("equipos.db");
-
-		ICriterion crit = new And()
-				.add(Where.gt("edad", 20))
-				.add(Where.equal("deporte", "fútbol"));
-		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
-		query.orderByAsc("nombre");
+		ICriterion crit = new Or().add(Where.equal("pais", "FRANCIA")).add(Where.equal("deporte", "baloncesto"));
+		IQuery query = new CriteriaQuery(Jugador.class, crit);
 		
 		Objects<Jugador> jugadores = odb.getObjects(query);
-		for (Jugador e: jugadores) {
-			System.out.println(e);
+		while(jugadores.hasNext()) {
+			System.out.println(jugadores.next());
 		}
 		odb.close();
 	}
 	
-	public static void getFrenchOrBasketPlayers() {
+	public static void saveNewJugador() {
+		ODB odb = ODBFactory.open("equipos.db");
+		Jugador j = new Jugador("Donald", "MMA", 34, new Paises(67, "Irlanda"));
+		
+		odb.store(j);
+		odb.commit();
+		odb.close();
+	}
+	
+	public static void getJugadorFromOID(int i) {
 		ODB odb = ODBFactory.open("equipos.db");
 		
-		ICriterion crit = new Or()
-				.add(Where.equal("pais.nombrepais", "FRANCIA"))
-				.add(Where.equal("deporte", "baloncesto"));
-		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
-		query.orderByAsc("nombre");
+		OID oid = OIDFactory.buildObjectOID(i);
+		Jugador j = (Jugador) odb.getObjectFromId(oid);
 		
-		Objects<Jugador> jugadores = odb.getObjects(query);
-		for (Jugador e: jugadores) {
-			System.out.println(e);
+		System.out.println("Jugador con oid " + i + ": " + j);
+		odb.close();
+	}
+	
+	public static void getTotalPlayersBySport() {
+		ODB odb = ODBFactory.open("equipos.db");
+		Values values = odb.getValues(new ValuesCriteriaQuery(Jugador.class)
+				.field("deporte").count("nombre").groupBy("deporte"));
+		
+		while (values.hasNext()) {
+			ObjectValues ov = values.next();
+			System.out.println(ov.getByAlias("deporte") + "->" + ov.getByIndex(1));
 		}
 		odb.close();
 	}
 	
-	public static void getPlayersBySport() {
+	public static void getAvgPlayersAge() {
 		ODB odb = ODBFactory.open("equipos.db");
 		
-		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class)
-				.field("deporte")
-				.count("nombre")
-				.groupBy("deporte"));
+		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class).avg("edad"));
 		
-		while (vals.hasNext()) {
-			ObjectValues ov = (ObjectValues) vals.next();
-			System.out.println("Deporte->" + ov.getByAlias("deporte") + ":" + ov.getByIndex(1));
-		}
+		System.out.println("Edad media de los jugadores: " + vals.getFirst());
 		odb.close();
 	}
 	
-	public static void getAvgAge() {
+	public static void getMinMaxPlayersAge() {
 		ODB odb = ODBFactory.open("equipos.db");
 		
-		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class)
-				.avg("edad"));
+		Values v1 = odb.getValues(new ValuesCriteriaQuery(Jugador.class).max("edad"));
+		Values v2 = odb.getValues(new ValuesCriteriaQuery(Jugador.class).min("edad"));
 		
-		while (vals.hasNext()) {
-			ObjectValues ov = (ObjectValues) vals.next();
-			System.out.println("Edad media de los jugadores: " + ov.getByIndex(0));
-		}
+		System.out.println("Edad maxima " + v1.getFirst() + "\n" + "Edad minima "+ v2.getFirst());
 		odb.close();
 	}
 	
-	public static void getFootballBetween18and25() {
+	public static void getFootballBtw18And25() {
 		ODB odb = ODBFactory.open("equipos.db");
-
 		ICriterion crit = new And()
 				.add(Where.equal("deporte", "fútbol"))
 				.add(Where.ge("edad", 18))
 				.add(Where.le("edad", 25));
-		
-		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
-		query.orderByAsc("nombre");
-		
+		IQuery query = new CriteriaQuery(Jugador.class, crit);
 		Objects<Jugador> jugadores = odb.getObjects(query);
-		for (Jugador e: jugadores) {
-			System.out.println(e);
+		
+		while (jugadores.hasNext()) {
+			System.out.println(jugadores.next());
 		}
 		odb.close();
 	}
 	
-	public static void getBasketUnder22() {
+	public static void getFrenchOrIta() {
+		ODB odb = ODBFactory.open("equipos.db");
+		ICriterion crit = new Or()
+				.add(Where.equal("pais.nombrepais", "FRANCIA"))
+				.add(Where.equal("pais.nombrepais", "ITALIA"));
+		
+		Values values = odb.getValues(new ValuesCriteriaQuery(Jugador.class, crit)
+				.field("nombre", "jugador").field("pais.nombrepais", "pais"));
+		
+		while (values.hasNext()) {
+			ObjectValues ov = values.next();
+			System.out.println(ov.getByAlias("jugador") + ":" + ov.getByAlias("pais"));
+		}
+		odb.close();
+	}
+	
+	public static void getBasketUnder23() {
 		ODB odb = ODBFactory.open("equipos.db");
 		
 		ICriterion crit = new And()
 				.add(Where.equal("deporte", "baloncesto"))
-				.add(Where.not(Where.gt("edad", 22)));
+				.add(Where.le("edad", 22));
 		
-		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
-		query.orderByAsc("nombre");
+		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class, crit).field("nombre").field("edad"));
 		
-		Objects<Jugador> jugadores = odb.getObjects(query);
-		for (Jugador e: jugadores) {
-			System.out.println(e);
+		while (vals.hasNext()) {
+			ObjectValues ov = vals.next();
+			System.out.println(ov.getByAlias("nombre") + ":" + ov.getByAlias("edad"));
 		}
 		odb.close();
 	}
 	
-	public static void getFrenchOrTennis() {
+	public static void getFrenchOrTennisCount() {
 		ODB odb = ODBFactory.open("equipos.db");
-		
 		ICriterion crit = new Or()
-				.add(Where.equal("deporte", "tenis"))
-				.add(Where.equal("pais.nombrepais", "FRANCIA"));
+				.add(Where.equal("pais.nombrepais", "FRANCIA"))
+				.add(Where.equal("deporte", "fútbol"));
 		
-		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
-		Values vals = odb.getValues(new ValuesCriteriaQuery(query)
+		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class, crit)
 				.count("nombre"));
 		
-		System.out.println("Total jugadores: " + vals.next().getByIndex(0)); 
-
-		odb.close();
-	}
-	
-	public static void getMinMaxAge() {
-		ODB odb = ODBFactory.open("equipos.db");
-		
-		Values val1 = odb.getValues(new ValuesCriteriaQuery(Jugador.class).max("edad", "edad_max"));
-		ObjectValues eMax = val1.nextValues();
-		Values val2 = odb.getValues(new ValuesCriteriaQuery(Jugador.class).min("edad", "edad_min"));
-		ObjectValues eMin = val2.nextValues();
-		
-		System.out.println("Edad minima: " + eMin.getByAlias("edad_min") + " \nEdad maxima: " + eMax.getByAlias("edad_max")); 
-		
-		odb.close();
-	}
-	
-	public static void getFrenchOrItalian() {
-		ODB odb = ODBFactory.open("equipos.db");
-		
-		ICriterion crit = new Or()
-				.add(Where.equal("pais.nombrepais", "ITALIA"))
-				.add(Where.equal("pais.nombrepais", "FRANCIA"));
-		
-		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
-		query.orderByAsc("nombre");
-		
-		Objects<Jugador> jugadores = odb.getObjects(query);
-		for (Jugador j:jugadores) {
-			System.out.println(j); 
+		while (vals.hasNext()) {
+			ObjectValues ov = vals.next();
+			System.out.println("Jugadores franceses o que practican tenis: " + ov.getByIndex(0));
 		}
 		
 		odb.close();
 	}
 	
-	public static void getAvgTennisOrMoreThan22() {
+	public static void getAvgTenisOrAbove22() {
 		ODB odb = ODBFactory.open("equipos.db");
-		
 		ICriterion crit = new Or()
 				.add(Where.equal("deporte", "tenis"))
 				.add(Where.gt("edad", 22));
 		
-		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
-		Values vals = odb.getValues(new ValuesCriteriaQuery(query)
-				.avg("edad"));
+		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class, crit).avg("edad"));
+		ObjectValues ov = vals.getFirst();
 		
-		System.out.println("Edad media de jugadores de tenis o mayores de 22: " + vals.next().getByIndex(0)); 
+		System.out.println("Edad media: " + ov);
 		
 		odb.close();
 	}
+	
+	//====================================================//
+//	
+//	public static void getPlayersAndCountries() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		Objects<Jugador> jugadores = null;
+//		
+//		jugadores = odb.getObjects(Jugador.class);
+//		while(jugadores.hasNext()) {
+//			System.out.println(jugadores.next());
+//		}
+//		
+//		odb.close();
+//	}
+//	
+//	public static void getFootballAbove20() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//
+//		ICriterion crit = new And()
+//				.add(Where.gt("edad", 20))
+//				.add(Where.equal("deporte", "fútbol"));
+//		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
+//		query.orderByAsc("nombre");
+//		
+//		Objects<Jugador> jugadores = odb.getObjects(query);
+//		for (Jugador e: jugadores) {
+//			System.out.println(e);
+//		}
+//		odb.close();
+//	}
+//	
+//	public static void getFrenchOrBasketPlayers() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		ICriterion crit = new Or()
+//				.add(Where.equal("pais.nombrepais", "FRANCIA"))
+//				.add(Where.equal("deporte", "baloncesto"));
+//		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
+//		query.orderByAsc("nombre");
+//		
+//		Objects<Jugador> jugadores = odb.getObjects(query);
+//		for (Jugador e: jugadores) {
+//			System.out.println(e);
+//		}
+//		odb.close();
+//	}
+//	
+//	public static void getPlayersBySport() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class)
+//				.field("deporte")
+//				.count("nombre")
+//				.groupBy("deporte"));
+//		
+//		while (vals.hasNext()) {
+//			ObjectValues ov = (ObjectValues) vals.next();
+//			System.out.println("Deporte->" + ov.getByAlias("deporte") + ":" + ov.getByIndex(1));
+//		}
+//		odb.close();
+//	}
+//	
+//	public static void getAvgAge() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		Values vals = odb.getValues(new ValuesCriteriaQuery(Jugador.class)
+//				.avg("edad"));
+//		
+//		while (vals.hasNext()) {
+//			ObjectValues ov = (ObjectValues) vals.next();
+//			System.out.println("Edad media de los jugadores: " + ov.getByIndex(0));
+//		}
+//		odb.close();
+//	}
+//	
+//	public static void getFootballBetween18and25() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//
+//		ICriterion crit = new And()
+//				.add(Where.equal("deporte", "fútbol"))
+//				.add(Where.ge("edad", 18))
+//				.add(Where.le("edad", 25));
+//		
+//		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
+//		query.orderByAsc("nombre");
+//		
+//		Objects<Jugador> jugadores = odb.getObjects(query);
+//		for (Jugador e: jugadores) {
+//			System.out.println(e);
+//		}
+//		odb.close();
+//	}
+//	
+//	public static void getBasketUnder22() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		ICriterion crit = new And()
+//				.add(Where.equal("deporte", "baloncesto"))
+//				.add(Where.not(Where.gt("edad", 22)));
+//		
+//		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
+//		query.orderByAsc("nombre");
+//		
+//		Objects<Jugador> jugadores = odb.getObjects(query);
+//		for (Jugador e: jugadores) {
+//			System.out.println(e);
+//		}
+//		odb.close();
+//	}
+//	
+//	public static void getFrenchOrTennis() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		ICriterion crit = new Or()
+//				.add(Where.equal("deporte", "tenis"))
+//				.add(Where.equal("pais.nombrepais", "FRANCIA"));
+//		
+//		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
+//		Values vals = odb.getValues(new ValuesCriteriaQuery(query)
+//				.count("nombre"));
+//		
+//		System.out.println("Total jugadores: " + vals.next().getByIndex(0)); 
+//
+//		odb.close();
+//	}
+//	
+//	public static void getMinMaxAge() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		Values val1 = odb.getValues(new ValuesCriteriaQuery(Jugador.class).max("edad", "edad_max"));
+//		ObjectValues eMax = val1.nextValues();
+//		Values val2 = odb.getValues(new ValuesCriteriaQuery(Jugador.class).min("edad", "edad_min"));
+//		ObjectValues eMin = val2.nextValues();
+//		
+//		System.out.println("Edad minima: " + eMin.getByAlias("edad_min") + " \nEdad maxima: " + eMax.getByAlias("edad_max")); 
+//		
+//		odb.close();
+//	}
+//	
+//	public static void getFrenchOrItalian() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		ICriterion crit = new Or()
+//				.add(Where.equal("pais.nombrepais", "ITALIA"))
+//				.add(Where.equal("pais.nombrepais", "FRANCIA"));
+//		
+//		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
+//		query.orderByAsc("nombre");
+//		
+//		Objects<Jugador> jugadores = odb.getObjects(query);
+//		for (Jugador j:jugadores) {
+//			System.out.println(j); 
+//		}
+//		
+//		odb.close();
+//	}
+//	
+//	public static void getAvgTennisOrMoreThan22() {
+//		ODB odb = ODBFactory.open("equipos.db");
+//		
+//		ICriterion crit = new Or()
+//				.add(Where.equal("deporte", "tenis"))
+//				.add(Where.gt("edad", 22));
+//		
+//		CriteriaQuery query = new CriteriaQuery(Jugador.class, crit);
+//		Values vals = odb.getValues(new ValuesCriteriaQuery(query)
+//				.avg("edad"));
+//		
+//		System.out.println("Edad media de jugadores de tenis o mayores de 22: " + vals.next().getByIndex(0)); 
+//		
+//		odb.close();
+//	}
 }
